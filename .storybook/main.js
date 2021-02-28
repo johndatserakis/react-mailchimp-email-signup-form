c871811b3deed319daf747fe86ffc3704cd7567b
John Datserakis <johndatserakis@gmail.com>
2021-02-28 01:47:01 -0500

    Update Storybook css handling

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,13 +7,13 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
   ],
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
+      test: /\.s(a|c)ss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader']
     });
     return config;
   },
